6a81453f92781e278e2890102eaef6837b3aa38b
lei-hsia <lx2196@columbia.edu>
2020-04-13 10:12:10 +0800

    lock+condition进行生产者消费者模型，精准唤醒

--- src/main/java/com/lei/concurrentandjvm/jmmjvmjucatomiclock20410/SyncAndReentrantLockDemo.java
+++ src/main/java/com/lei/concurrentandjvm/jmmjvmjucatomiclock20410/SyncAndReentrantLockDemo.java
@@ -1,16 +1,72 @@
 package com.lei.concurrentandjvm.jmmjvmjucatomiclock20410;
 
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 class ShareResources {
     private int flag = 1;
     private Lock lock = new ReentrantLock();
+    private Condition condition1 = lock.newCondition();
+    private Condition condition2 = lock.newCondition();
+    private Condition condition3 = lock.newCondition();
 
     public void print2() {
         lock.lock();
         try {
-            //
+            // 判断
+            while (flag != 1) {
+                condition1.await();
+            }
+            // 干活
+            for (int i = 0; i < 2; i++) {
+                System.out.println(Thread.currentThread().getName()+"\t"+flag);
+            }
+            // 唤醒
+            flag++; // 改变状态
+            condition2.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print3() {
+        lock.lock();
+        try {
+            // 判断
+            while (flag != 2) {
+                condition2.await();
+            }
+            // 干活
+            for (int i = 0; i < 3; i++) {
+                System.out.println(Thread.currentThread().getName()+"\t"+flag);
+            }
+            // 唤醒
+            flag++; // 改变状态
+            condition3.signal();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void print4() {
+        lock.lock();
+        try {
+            // 判断
+            while (flag != 3) {
+                condition3.await();
+            }
+            // 干活
+            for (int i = 0; i < 4; i++) {
+                System.out.println(Thread.currentThread().getName()+"\t"+flag);
+            }
+            // 唤醒
+            flag = 1; // 改变状态
+            condition1.signal();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -30,6 +86,21 @@ class ShareResources {
 // 要求: 多线程按顺序调用: A2次->B3次->C4次:
 public class SyncAndReentrantLockDemo {
     public static void main(String[] args) {
+        ShareResources shareResources = new ShareResources();
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println(">>>>>>>> the"+i+"th round <<<<<<<<<<");
+            new Thread(() -> {
+                shareResources.print2();
+            }, "AA").start();
+
+            new Thread(() -> {
+                shareResources.print3();
+            }, "BB").start();
+
+            new Thread(() -> {
+                shareResources.print4();
+            }, "CC").start();
+        }
     }
 }
