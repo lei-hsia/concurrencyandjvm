14f65b70bb61698bf9c7f35f8d9f7e379c72f42b
lei-hsia <leileetcodekaggle@163.com>
2020-06-28 15:46:40 +0800

    代理模式: MapperProxyFactory>MapperProxy>invoke>sqlSession>executor.execute>prepareStatement

--- src/main/java/com/lei/mybatis/MybatisDesignPatterns.java
+++ src/main/java/com/lei/mybatis/MybatisDesignPatterns.java
@@ -1,8 +1,19 @@
 package com.lei.mybatis;
 
 import com.sun.tools.javac.util.Log;
+import org.apache.ibatis.binding.MapperMethod;
+import org.apache.ibatis.binding.MapperProxy;
+import org.apache.ibatis.reflection.ExceptionUtil;
+import org.apache.ibatis.session.SqlSession;
 
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MybatisDesignPatterns {
     public static void main(String[] args) {
@@ -12,6 +23,7 @@ public class MybatisDesignPatterns {
     }
 }
 
+/* 单例模式 */
 // thread-level
 class ErrorContext {
     private static final ThreadLocal<ErrorContext> LOCAL = new ThreadLocal<>();
@@ -39,3 +51,66 @@ class LogFactory {
 
 }
 
+/* 代理模式:借助MapperProxy; 而proxy用MapperProxyFactory工厂模式创建出来的 */
+class MapperProxyFactory<T> {
+    private final Class<T> mapperInterface;
+    private final Map<Method, MapperMethod> methodCache = new ConcurrentHashMap();
+
+    public MapperProxyFactory(Class<T> mapperInterface) {
+        this.mapperInterface = mapperInterface;
+    }
+
+    public Class<T> getMapperInterface() {
+        return this.mapperInterface;
+    }
+
+    public Map<Method, MapperMethod> getMethodCache() {
+        return this.methodCache;
+    }
+
+    protected T newInstance(MapperProxy<T> mapperProxy) {
+        return (T) Proxy.newProxyInstance(this.mapperInterface.getClassLoader(), new Class[]{this.mapperInterface}, mapperProxy);
+    }
+
+    public T newInstance(SqlSession sqlSession) {
+        MapperProxy<T> mapperProxy = new MapperProxy(sqlSession, this.mapperInterface, this.methodCache);
+        return this.newInstance(mapperProxy);
+    }
+}
+
+/*  非常典型的，该MapperProxy类实现了InvocationHandler接口，并且实现了该接口的invoke方法。
+class MapperProxy<T> implements InvocationHandler, Serializable {
+    private static final long serialVersionUID = -6424540398559729838L;
+    private static final int ALLOWED_MODES = 15;
+    private final SqlSession sqlSession;
+    private final Class<T> mapperInterface;
+    private final Map<Method, MapperMethod> methodCache;
+
+    public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface, Map<Method, MapperMethod> methodCache) {
+        this.sqlSession = sqlSession;
+        this.mapperInterface = mapperInterface;
+        this.methodCache = methodCache;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        try {
+            if (Object.class.equals(method.getDeclaringClass())) {
+                return method.invoke(this, args);
+            }
+
+            if (method.isDefault()) {
+                if (privateLookupInMethod == null) {
+                    return this.invokeDefaultMethodJava8(proxy, method, args);
+                }
+
+                return this.invokeDefaultMethodJava9(proxy, method, args);
+            }
+        } catch (Throwable var5) {
+            throw ExceptionUtil.unwrapThrowable(var5);
+        }
+
+        MapperMethod mapperMethod = this.cachedMapperMethod(method);
+        return mapperMethod.execute(this.sqlSession, args);
+    }
+}
+*/
