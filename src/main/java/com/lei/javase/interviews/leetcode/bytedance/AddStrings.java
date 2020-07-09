941fea595135318ddb5ca422d1bcdf58761336f7
lei-hsia <leileetcodekaggle@163.com>
2020-07-09 10:01:47 +0800

    lc102 level order traversal

--- src/main/java/com/lei/javase/interviews/leetcode/bytedance/AddStrings.java
+++ src/main/java/com/lei/javase/interviews/leetcode/bytedance/AddStrings.java
@@ -13,4 +13,3 @@ public class AddStrings {
         return sb.reverse().toString();
     }
 }
-}
---
+++ src/main/java/com/lei/javase/interviews/leetcode/bytedance/BTlevelOrderTraversal.java
@@ -0,0 +1,55 @@
+package com.lei.javase.interviews.leetcode.bytedance;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+       this.val = val;
+       this.left = left;
+       this.right = right;
+    }
+}
+
+public class BTlevelOrderTraversal {
+
+    public List<List<Integer>> recursiveLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList();
+        dfs(res, root, 0);
+        return res;
+    }
+    public void dfs(List<List<Integer>> res, TreeNode node, int level) {
+        if (node == null) return;
+        if (level >= res.size()) res.add(new ArrayList());
+        res.get(level).add(node.val);
+        if (node.left != null) dfs(res, node.left, level+1);
+        if (node.right != null) dfs(res, node.right, level+1);
+    }
+
+    public List<List<Integer>> iterativeLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList();
+        if (root == null) return res;
+        Queue<TreeNode> q = new LinkedList();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int count = q.size();
+            List<Integer> arr = new ArrayList();
+            for (int i = 0; i < count; ++i) {
+                // 看队列首位的元素，然后把此元素的左右子节点都加入队列
+                if (q.peek().left != null) q.offer(q.peek().left);
+                if (q.peek().right != null) q.offer(q.peek().right);
+                arr.add(q.poll().val);
+            }
+            res.add(arr);
+        }
+        return res;
+    }
+}
