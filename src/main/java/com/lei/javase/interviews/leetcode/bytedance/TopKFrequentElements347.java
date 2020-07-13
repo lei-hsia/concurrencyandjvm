26e70fa7d564c9997667708ca61a8e6588211d10
lei-hsia <leileetcodekaggle@163.com>
2020-07-13 17:14:13 +0800

    TopKFrequentElements: O(N logN)用priority queue

--- src/main/java/com/lei/javase/interviews/leetcode/bytedance/TopKFrequentElements347.java
+++ src/main/java/com/lei/javase/interviews/leetcode/bytedance/TopKFrequentElements347.java
@@ -12,7 +12,29 @@ import java.util.Queue;
 * */
 public class TopKFrequentElements347 {
 
+    /* Use a priority queue. O(N log N)     */
+    public int[] topKFrequent(int[] nums, int k) {
+        if (nums.length == k) return nums;
 
+        Map<Integer, Integer> count = new HashMap();
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+
+        // 频率从小到大: 保证heap大小为k, 每次poll都是poll频率小的，剩下k个就是top k
+        Queue<Integer> heap = new PriorityQueue<>((n1, n2) -> count.get(n1) - count.get(n2));
+
+        for (int n : count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) heap.poll();
+        }
+
+        int[] res = new int[k];
+        for (int i = k-1; i>=0; --i) {
+            res[i] = heap.poll();
+        }
+        return res;
+    }
 
 
 }
